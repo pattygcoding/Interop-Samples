7f62216cf1d77041de48bf5f010439534de00f33
Patrick Goodwin <pattygsocials@gmail.com>
2024-08-26 16:24:30 -0400

    Create main.hs

---
+++ Haskell/Haskell with Scala/main.hs
@@ -0,0 +1,24 @@
+import System.IO
+import System.Process
+
+main :: IO ()
+main = do
+    -- Define the list of numbers
+    let numbers = [10.0, 20.0, 30.0, 40.0, 50.0]
+    
+    -- Write the numbers to a file for the Scala program to read
+    writeFile "numbers.txt" (unwords $ map show numbers)
+    
+    -- Call the Scala program to calculate the sum
+    _ <- system("scala SumCalculator")
+
+    -- Read the sum from the result file
+    sumResult <- readFile "result.txt"
+    let sumValue = read sumResult :: Double
+
+    -- Calculate the average
+    let count = fromIntegral (length numbers)
+    let average = sumValue / count
+
+    -- Print the average
+    putStrLn $ "The average is: " ++ show average
